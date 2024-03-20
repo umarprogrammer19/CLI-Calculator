3917caf2187bb07da588c1ff9374277aca48b3f5
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-03-20 09:52:41 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answer = await inquirer.prompt([{
+    message: chalk.green("Enter Your First Number"),
+    type: "number",
+    name: "firstNumber"
+}, {
+    message: chalk.green("Enter Your Second Number"),
+    type: "number",
+    name: "secondNumber"
+}, {
+    message: chalk.green("Enter Your Opearator"),
+    type: "list",
+    name: "opearator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+}]);
+
+if (answer.opearator === "Addition") {
+    console.log(chalk.yellow(`Your Answer is ${answer.firstNumber + answer.secondNumber}`));
+} else if (answer.opearator === "Subtraction") {
+    console.log(chalk.yellow(`Your Answer is ${answer.firstNumber - answer.secondNumber}`));
+} else if (answer.opearator === "Multiplication") {
+    console.log(chalk.yellow(`Your Answer is ${answer.firstNumber * answer.secondNumber}`));
+} else if (answer.opearator === "Division") {
+    console.log(chalk.yellow(`Your Answer is ${answer.firstNumber / answer.secondNumber}`));
+} else {
+    console.log(chalk.red("Please Enter Correct Value"));
+}
